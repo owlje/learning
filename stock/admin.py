b74f71a5d7d4401242940c90fa8403ad680ffd28
owlje <korolekko@yandex.ru>
2023-01-11 06:16:44 +0300

    Lab 2

--- stock/admin.py
+++ stock/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from stock.models import Stock
-from stock.models import Currency
+from stock.models import (Stock, Currency, Account,
+                          AccountCurrency, AccountStock)
+
 
 @admin.register(Stock)
 class StockAdmin(admin.ModelAdmin):
@@ -10,3 +11,23 @@ class StockAdmin(admin.ModelAdmin):
 @admin.register(Currency)
 class CurrencyAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(AccountCurrency)
+class AccountCurrencyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AccountStock)
+class AccountStockAdmin(admin.ModelAdmin):
+    pass
+
+class AccountCurrencyInline(admin.TabularInline):
+    model = AccountCurrency
+
+
+class AccountStockInline(admin.TabularInline):
+    model = AccountStock
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    inlines = [AccountCurrencyInline, AccountStockInline]
